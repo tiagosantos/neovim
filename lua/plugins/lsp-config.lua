d68b85f7c9ee647912bb13ce340e88875fcfc779
Carlos Tiago <ontiago@gmail.com>
2024-05-28 16:54:55 -0300

    add linters for JS, Lua And Ruby

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,8 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "rubocop" }
+        --ensure_installed = { "lua_ls", "tsserver", "rubocop" }
+        auto_install = true
       })
     end
   },
