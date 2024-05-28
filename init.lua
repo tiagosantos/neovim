022d62c1959737b7409cd50f25624e226f36574d
Carlos Tiago <ontiago@gmail.com>
2024-05-28 12:30:08 -0300

    add neotree

--- init.lua
+++ init.lua
@@ -18,13 +18,30 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000
+  },
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.6',
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
-  { 'wakatime/vim-wakatime', lazy = false },
-  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' }
+  { 'wakatime/vim-wakatime', 
+    lazy = false 
+  },
+  { 'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate' 
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    }
+  }
 }
 local opts = {}
 
@@ -32,6 +49,8 @@ require("lazy").setup(plugins, opts)
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {}) -- requires ripgrep: brew install ripgrep
+
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
 
 local config = require("nvim-treesitter.configs")
 config.setup({
