a8380723814cf1032501ba9b447bc773f8fdc81c
Carlos Tiago <ontiago@gmail.com>
2024-05-28 13:44:21 -0300

    split plugins and configs

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return { 
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_instaled = { "lua", "javascript", "dart", "html", "ruby", "vue" },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+}
+
