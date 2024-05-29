9c60ec474b94a855586edf1b76c4c59ec051b603
Carlos Tiago <ontiago@gmail.com>
2024-05-29 10:16:15 -0300

    add screens configs

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,6 +7,26 @@ vim.g.background = "light"
 
 vim.opt.swapfile = false
 
+vim.wo.number = true
+vim.opt.relativenumber = true
+vim.opt.smartindent = true
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.colorcolumn = "80"
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
@@ -14,5 +34,4 @@ vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-vim.wo.number = true
 
