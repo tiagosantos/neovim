3ab018a262b791cf440dd5271e45b178e6efff13
Carlos Tiago <ontiago@gmail.com>
2024-05-29 11:34:11 -0300

    add arrow

---
+++ lua/plugins/arrow.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "otavioschwanck/arrow.nvim",
+    event = "VeryLazy",
+    opts = {
+      show_icons = true,
+      leader_key = ";", -- Recommended to be a single key
+      always_show_path = true,
+    },
+  },
+}
