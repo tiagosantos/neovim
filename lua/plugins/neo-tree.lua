d23d838fb762eccb25fdfde7acc3761da4d4559a
Carlos Tiago <ontiago@gmail.com>
2024-05-29 15:31:31 -0300

    show dotfiles on neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,31 +1,32 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-    "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+		"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	config = function()
+		local neotree = require("neo-tree")
+		neotree.setup({
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					show_hidden_count = true,
+					hide_dotfiles = false,
+					hide_gitignored = true,
+					hide_by_name = {
+						".git",
+						".DS_Store",
+						"thumbs.db",
+					},
+					never_show = {},
+				},
+			},
+		})
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
 
-  },
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        show_hidden_count = true,
-        hide_dotfiles = false,
-        hide_gitignored = true,
-        hide_by_name = {
-          '.git',
-          '.DS_Store',
-          'thumbs.db',
-        },
-        never_show = {},
-      },
-    }
-  },
-  config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', opts)
-    vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-  end
+		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+	end,
 }
