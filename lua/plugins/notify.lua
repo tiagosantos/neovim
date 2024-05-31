15eb3228e5ffb3791575385b8ea38bfb945db4d4
Carlos Tiago <ontiago@gmail.com>
2024-05-31 19:08:11 -0300

    fix notify

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,13 +1,32 @@
 return {
-
-  {
-    "rcarriga/nvim-notify",
-    lazy = false,
-    priority = 900,
-    config = function(_, opts)
-      vim.notify = require("notify")
-      require("notify").setup(opts)
-    end,
-  },
+	{
+		"rcarriga/nvim-notify",
+		lazy = false,
+		priority = 900,
+		keys = {
+			{
+				"<leader>un",
+				function()
+					require("notify").dismiss({ silent = true, pending = true })
+				end,
+				desc = "Dismiss All Notifications",
+			},
+		},
+		opts = {
+			stages = "static",
+			timeout = 3000,
+			max_height = function()
+				return math.floor(vim.o.lines * 0.75)
+			end,
+			max_width = function()
+				return math.floor(vim.o.columns * 0.75)
+			end,
+			on_open = function(win)
+				vim.api.nvim_win_set_config(win, { zindex = 100 })
+			end,
+		},
+		init = function()
+			vim.notify = require("notify")
+		end,
+	},
 }
-
